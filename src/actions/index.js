19e776250f7f16aaff9532e4168fd89a9341ef00
Spencer Haggard <spencer.timekeeper99@gmail.com>
2017-11-04 18:08:34 -0400

    setting up

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+import axios from 'asios';
+
+export const FETCH_POSTS = 'fetch_posts';
+
+const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
+const API_KEY = '?key=KEEPEROFTIME';
+
+export function fetchPost() {
+	const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
+
+	return {
+		type: FETCH_POSTS
+		payload: request
+	};
+}
